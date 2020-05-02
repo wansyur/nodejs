645cd0e98b21a1504ee4fdcac1926c23b2430811
wansyur <iwan.wansyur@gmail.com>
2020-05-03 06:58:42 +0700

    commit 3

--- materi3/controller.js
+++ materi3/controller.js
@@ -16,3 +16,67 @@ exports.blog = function(req, res) {
 exports.index = function(req, res) {
     response.ok("Hello from the Node JS RESTful side!", res)
 };
+
+exports.findBlog = function(req, res) {
+    
+    var blog_id = req.params.blog_id;
+
+    connection.query('SELECT * FROM blog where id_blog = ?',
+    [ blog_id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.createBlog = function(req, res) {
+    
+    var nama_blog = req.body.nama_blog;
+    var detail = req.body.detail;
+    
+
+    connection.query('INSERT INTO blog (nama_blog, detail) values (?,?)',
+    [ nama_blog, detail ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok("Berhasil menambahkan blog!", res)
+        }
+    });
+};
+
+exports.updateBlog = function(req, res) {
+    
+    var blog_id = req.body.blog_id;
+    var nama_blog = req.body.nama_blog;
+    var detail = req.body.detail;
+
+    connection.query('UPDATE blog SET nama_blog = ?, detail = ? WHERE id_blog = ?',
+    [ nama_blog, detail, blog_id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok("Berhasil merubah blog!", res)
+        }
+    });
+};
+
+exports.deleteBlog = function(req, res) {
+    
+    var blog_id = req.body.blog_id;
+
+    connection.query('DELETE FROM blog WHERE id_blog = ?',
+    [ blog_id ], 
+    function (error, rows, fields){
+        if(error){
+            console.log(error)
+        } else{
+            response.ok("Berhasil menghapus blog!", res)
+        }
+    });
+};
--- materi3/routes.js
+++ materi3/routes.js
@@ -8,4 +8,15 @@ module.exports = function(app) {
 
     app.route('/blog')
         .get(todoList.blog); 
+    app.route('/blog/:blog_id')
+        .get(todoList.findBlog);
+
+    app.route('/blog')
+        .post(todoList.createBlog);
+
+    app.route('/blog')
+        .put(todoList.updateBlog);
+    
+    app.route('/blog')
+        .delete(todoList.deleteBlog);
 };
