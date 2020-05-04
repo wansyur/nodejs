ced827353618fb250cbb0c8e4e10f283a4bc8e7a
wansyur <iwan.wansyur@gmail.com>
2020-05-04 08:49:19 +0700

    commit 4 a
    Signed-off-by: wansyur <iwan.wansyur@gmail.com>

--- materi3/server.js
+++ materi3/server.js
@@ -3,12 +3,25 @@ var express = require('express'),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser'), 
     controller = require('./controller');
+const hbs = require('hbs');
+
+const path = require('path');
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 var routes = require('./routes');
 routes(app);
+
+//route untuk manampilkan form
+app.get('/postblog',(req, res) => {
+    //render file form.hbs
+    res.render('form');
+  });
 
 app.listen(port);
 console.log('Learn Node JS With Kiddy, RESTful API server started on: ' + port);
